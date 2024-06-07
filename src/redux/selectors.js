44246a96432147e725fea70404f3708346dea11c
DekamaxX <dobosz.mateusz1994@gmail.com>
2024-06-07 08:19:54 +0200

    friday fixes app problem

--- src/redux/selectors.js
+++
@@ -1,8 +0,0 @@
-export const selectContacts = state => state.contacts;
-
-export const selectFilters = state => state.filter;
-
-export const selectIsLoading = state =>
-  state.contacts.isLoading;
-
-export const selectError = state => state.contacts.error;
